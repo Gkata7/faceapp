2bd45c15713e05d31c1736468531af16958295d2
Garrett <gkatashima@gmail.com>
2019-03-28 15:06:48 -0700

    updated links

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
-          fetch('http://localhost:3000/imageUrl', {
+          fetch('https://limitless-tor-21666.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
         .then(response => response.json)
         .then(response => {
           if(response){
-            fetch('http://localhost:3000/image', {
+            fetch('https://limitless-tor-21666.herokuapp.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
