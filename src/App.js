04d1fefb5540d04d61504eeb22ba5c39196df6b2
Garrett <gkatashima@gmail.com>
2019-03-29 01:01:28 -0700

    update image

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-            input: this.state.input,
+              input: this.state.input,
           })
         })
         .then(response => response.json)
